7b7ed3c8c13024ca4860be88dffc7cb0e18ca4db
Bernat <socrattes@gmail.com>
2016-11-16 22:38:19 -1000

    Improve

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
         tasks: ['coffee:server']
       },
       pug: {
-        files: ["<%= basement.app %>/*.pug"],
+        files: ["<%= basement.app %>/**/*.pug"],
         tasks: ["pug:server"]
       },
       less: {
@@ -123,7 +123,7 @@ module.exports = function (grunt) {
           yuicompress: true
         },
         files: {
-          ".tmp/styles/main.css": "<%= basement.app %>/styles/main.less"
+          ".tmp/styles/styles.css": "<%= basement.app %>/styles/styles.less"
         }
       },
       dist: {
@@ -132,7 +132,7 @@ module.exports = function (grunt) {
           yuicompress: true
         },
         files: {
-          "<%= basement.dist %>/styles/main.css": "<%= basement.app %>/styles/main.less"
+          "<%= basement.dist %>/styles/styles.css": "<%= basement.app %>/styles/styles.less"
         }
       }
     },
@@ -147,7 +147,7 @@ module.exports = function (grunt) {
             '*.{ico,txt}',
             'scripts/**/*.js',
             'styles/fonts/**',
-            '*.html',
+            '*.{html,xml}',
             '.htaccess',
             'CNAME',
             'images/{,*/}*.{webp,gif,jpg,png,svg}'
